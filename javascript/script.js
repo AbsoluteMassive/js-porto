6c2bdd5b41cc1adfbce6886769cd907562092204
AbsoluteMassive <absolutemassive@duck.com>
2026-03-25 13:24:17 +0000

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,10 @@
 function forWater() {
-  console.log("Changing h1 and p using onclick using id");
-  var paragraphChange = document.getElementById("carrot");
-      paragraphChange.textContent = "this is a new paragraph";
+  var paragraphText = document.getElementById("carrot");
+      paragraphText.textContent = "this is a new paragraph";
 
-  var headingChange = document.getElementById("banana");
-      headingChange.textContent = "JavaScript test page";
+  var headingText = document.getElementById("banana");
+      headingText.textContent = "JavaScript test page";
 };
 
 /* var x = 5; var y = 12; var result = x + y; console.log(result); */
 // COMMENT 
-
-
-
